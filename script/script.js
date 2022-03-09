e611f3d6b95273f747589d5e874b44d7fd0d251b
Wagner Vinícius <84077567+WagnerVCF@users.noreply.github.com>
2022-03-08 22:44:09 -0300

    Atualização Calculadora

--- script/script.js
+++ script/script.js
@@ -1,5 +1,56 @@
-onload = () => {
-    document.querySelector('btn1'.oncli)
-}
-
-//https://www.youtube.com/watch?v=3VEbigWyakg
+function calcNum(num) {
+    if (typeof gvisor == 'undefined') {
+       document.calcform.visor.value = '';
+    }
+    document.calcform.visor.value = document.calcform.visor.value + num;
+    gvisor = 1;
+ }
+ 
+ function Limpar() {
+    document.calcform.visor.value = '';
+    delete gvalor;
+    delete goper;
+    delete gvisor;
+ }
+ 
+ function calcOper(oper, valor1, valor2) {
+    if (oper == "somar") {
+       var valor = parseFloat(valor1) + parseFloat(valor2);
+    } else {
+       if (oper == "subtrair") {
+          var valor = valor1 - valor2;
+       } else {
+          if (oper == "multiplicar") {
+             var valor = valor1 * valor2;
+          } else {
+             var valor = valor1 / valor2;
+          }
+       }
+    }
+ 
+    return(valor);
+ }
+ console.log(calcOper);
+ 
+ function calcParse(oper) {
+    var valor = document.calcform.visor.value;
+    delete gvisor;
+ 
+    if (typeof goper != 'undefined' && oper == 'resultado') {
+       gvalor = calcOper(goper, gvalor, valor);
+       document.calcform.visor.value = gvalor;
+       delete oper;
+       delete gvalor;
+       return(0);
+    }
+ 
+    if (typeof gvalor != 'undefined') {
+       gvalor = calcOper(goper, gvalor, valor);
+       goper = oper;
+       document.calcform.visor.value = gvalor;
+    } else {
+       gvalor = valor;
+       goper = oper;
+    }
+ 
+ }
